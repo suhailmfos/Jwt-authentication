654e183b0cb5164220a0a902b42feed9ed1b5880
Suhail <suhailmfos@gmail.com>
2024-01-19 16:03:19 +0530

    clean the code

--- Jwt-authentication/src/main/java/com/jwt/Bug.java
+++
@@ -1,9 +0,0 @@
-package com.jwt;
-
-public class Bug {
-
-	public Bug() {
-		// TODO Auto-generated constructor stub
-	}
-
-}
--- Jwt-authentication/src/main/java/com/jwt/JwtAuthenticationApplication.java
+++ Jwt-authentication/src/main/java/com/jwt/JwtAuthenticationApplication.java
@@ -9,16 +9,4 @@ public class JwtAuthenticationApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JwtAuthenticationApplication.class, args);
 	}
-
-//	@Bean
-//	public WebMvcConfigurer corsConfigurer() {
-//		return new WebMvcConfigurer() {
-//
-//			@Override
-//			public void addCorsMappings(CorsRegistry registry) {
-//				registry.addMapping("/**").allowedOrigins("http://localhost:3001");
-//			}
-//			
-//		};
-//	}
 }
